462dc066e868b3fc71b568376c8fb9c305259987
Thomas George <private@thomas-george.com>
2023-01-11 13:06:02 +0100

    Initial Commit

---
+++ Unit Testing Tip Calculator/Unit_Testing_Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_Testing_Tip_CalculatorApp.swift
+//  Unit Testing Tip Calculator
+//
+//  Created by Thomas George on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_Testing_Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
